dc40d5124cb9e994dc09a821240f4efd74a59080
Deogenes Maranan <maranandeogenes@gmail.com>
2023-04-27 13:40:55 +0800

    Trying to Complete the Calc

--- miscellaneous.py
+++ miscellaneous.py
@@ -57,3 +57,59 @@ class CarbonCalculator(CarbonCalculatorABC, ErrorHandler):
 
         # Convert to grams per day
         return ((electricity_emissions + lpg_emissions) / occupants / house_size_sq_ft) * 1000 / 30
+    
+    def calculate_transportation_emissions(self):
+        transportation_type = self.get_valid_option('''
+            Your form of transportation 
+                0 - Walking
+                1 - Private Vehicle
+                2 - Public Vehicle
+
+Response: ''', ['0', '1', '2'])
+
+        if transportation_type == '0':
+            transportation_co2e = 0  # No emissions for walking
+        elif transportation_type == '1':
+            passengers = self.get_int("Number of people in the vehicle: ")
+            distance = self.get_float("Distance of your transportation (km): ")
+            fuel_efficiency = self.get_float(
+                "What is the fuel efficiency of the vehicle (in km/L)? ")
+            fuel_type = input("What type of fuel does the vehicle use? (1 - Gasoline / 2 - Diesel) ")
+            emissions_factor = 2352.7 if fuel_type == '1' else 2639.4 if fuel_type == '2' else 0
+            transportation_co2e = (emissions_factor * distance / fuel_efficiency) / passengers
+        elif transportation_type == '2':
+            distance = self.get_float("Distance of your transportation (km): ")
+            transportation_co2e = 90 * distance  # 90 g CO2e/km/passenger on average for public transportation
+        else:
+            print("Sorry, we didn't understand your transportation type.")
+            transportation_co2e = 0
+
+        return transportation_co2e
+    
+    def calculate_food_emissions(self):
+        emissions_dict = {}
+        with open("food.txt") as f:
+            for line in f:
+                food, emissions = line.strip().split(":")
+                emissions_dict[food] = float(emissions)
+        food_items = []
+        while True:
+            food_item = input("Enter a food item you ate today (or 'done' if finished): ")
+            if food_item == 'done':
+                break
+            else:
+                food_items.append(food_item)
+
+        food_co2e = 0
+        for food_item in food_items:
+            if food_item in emissions_dict:
+                servings = super().get_float(f"Your servings of {food_item} (grams) ")
+                food_co2e += (servings * emissions_dict[food_item])
+            else:
+                print(f"Sorry, we don't have emissions data for {food_item}. Skipping...")
+        return food_co2e
+
+    def calculate_all(self, current_user):
+        total_emissions = self.calculate_housing_emissions() + self.calculate_transportation_emissions() + self.calculate_food_emissions()
+        with open(self.filename, 'a') as file:
+            file.write(total_emissions)
