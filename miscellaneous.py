204d86ceed899bb5f0ef6ecb542e3b11bed8619a
Deogenes Maranan <maranandeogenes@gmail.com>
2023-04-27 13:29:38 +0800

    includes Carbon Calculator class

--- miscellaneous.py
+++ miscellaneous.py
@@ -34,3 +34,26 @@ class ErrorHandler(ErrorHandlerABC):
             except ValueError:
                 print("Invalid input. Please enter a valid value.")
 
+class CarbonCalculator(CarbonCalculatorABC, ErrorHandler):
+
+    def __init__(self, current_user):
+        self.filename = f"user-{current_user}.txt"  # names '.txt' files for each user
+
+    def calculate_housing_emissions(self):  # Ask user for housing information
+
+        house_size_sq_m = self.get_float("Size of your house (square meters): ")
+        occupants = self.get_int("Number of occupants in your house: ")
+        electricity_use = self.get_float("Electric consumption per month (kWH): ")
+        cooking_fuel = self.get_int("Enter the type of cooking fuel (1-LPG, 2-Electric, 3-Bio): ")
+
+        lpg_emissions = 0
+        if cooking_fuel == 1:
+            lpg_use = self.get_float("Estimate the number of days your 11 kg LPG lasts: ")
+            lpg_emissions = (35 / lpg_use) * 30  # 35 CO2e = (30 per LPG cylinder) + (5 average CO2e of stove per Cylinder)
+
+        #  Formulas per month
+        house_size_sq_ft = house_size_sq_m * 10.764  # 1 sq m = 10.764 sq ft
+        electricity_emissions = electricity_use * 0.5  # 0.5 kg CO2e per kWH
+
+        # Convert to grams per day
+        return ((electricity_emissions + lpg_emissions) / occupants / house_size_sq_ft) * 1000 / 30
