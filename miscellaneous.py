5ba8b23e45b096b99ea9368f88c9972a1dcd5b25
Deogenes Maranan <maranandeogenes@gmail.com>
2023-04-27 12:59:44 +0800

    Docstrings for each abstract class

--- miscellaneous.py
+++ miscellaneous.py
@@ -1,36 +1,63 @@
 from abc import ABC, abstractmethod
 
 class ErrorHandler(ABC):
+    """
+    Abstract Base Class for Error Handling.
+    """
 
     @abstractmethod
     def get_valid_option(self, prompt: str, valid_options: list) -> str:
+        """
+        Checks if the user input is valid.
+        """
         pass
 
     @abstractmethod
     def get_float(self, prompt: str) -> float:
+        """
+        Checks if the user input is a float that is greater than 0.
+        """
         pass
 
     @abstractmethod
     def get_int(self, prompt: str) -> int:
+        """
+        Checks if the user input is a integer that is greater than 0.
+        """
         pass
 
 
 class CarbonCalculator(ABC):
+    """
+    Abstract Base Class for Calculating Carbon Footprint.
+    """
 
     @abstractmethod
     def calculate_housing_emissions(self):
+        """
+        Prompts user to give housing info to covert it into co2e.
+        """
         pass
 
     @abstractmethod
     def calculate_transportation_emissions(self):
+        """
+        Prompts user to give transportation info to covert it into co2e.
+        """
         pass
 
     @abstractmethod
     def calculate_food_emissions(self):
+        """
+        Prompts user to give food info to covert it into co2e.
+        """
         pass
 
     @abstractmethod
     def calculate_all(self, current_user):
+        """
+        Calculate the total co2e.
+        """
         pass
     
 class AccountManagerABC(ABC):
